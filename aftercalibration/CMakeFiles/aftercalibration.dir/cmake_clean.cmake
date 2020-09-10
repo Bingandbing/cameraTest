a7bcc830787648b562b43ae9946f980206a761eb
Bingandbing <46676950+Bingandbing@users.noreply.github.com>
2020-09-10 01:55:18 -0700

    Add files via upload

---
+++ aftercalibration/CMakeFiles/aftercalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aftercalibration.dir/aftercalibration.cpp.o"
+  "aftercalibration"
+  "aftercalibration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aftercalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
